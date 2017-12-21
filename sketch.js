3b9193748d2715e769692ecd71e432fe0bf9b994
InkFx <davidsantana409@yahoo.com>
2017-12-20 21:44:59 -0800

    Adding Functionality

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let submit;
+let imgURL;
+let img;
+let gifField;
+let gifResults;
+let url1 = 'http://api.giphy.com/v1/gifs/search?q=';
+let apiKey = '&api_key=jIAqh37hnPfdiwR4rYXgnBz829oPjub7';
+let searchTerm ;
+let url;
+
+function setup() {
+  noCanvas();
+
+
+  submit = createButton('Pick New GIF');
+  gifField = createInput('');
+
+  submit.position(20, 500)
+  gifField.position(250, 500);
+  submit.mousePressed(searchGIF);
+}
+
+function dataGet(data) {
+  imgURL = data.data[0].images.original.url;
+  //print(data.data[0].images.original.url);
+  img = createImg(imgURL,"UHHHHH PROBLEM HAPPEN :(");
+  img.html(imgURL);
+  img.position(windowHeight / 5 ,100);
+}
+
+
+function searchGIF() {
+  searchTerm = gifField.value();
+  url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=jIAqh37hnPfdiwR4rYXgnBz829oPjub7`;
+  loadJSON(url, dataGet);
+  print(searchTerm);
+}
